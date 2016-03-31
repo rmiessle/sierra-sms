6af52057e5fc0071f1443498558ddcb5119b7f23
R.C. Miessler <rmiessle@gettysburg.edu>
2016-03-31 15:01:53 -0400

    Update twilio-auth.php

--- sms/www/twilio/twilio-auth.php
+++ sms/www/twilio/twilio-auth.php
@@ -1,5 +1,5 @@
 <?php
 	// Twilio authentication credentials
-	$twilioSID="AC8008959617dfcb9ae730a41dffd68673";
-	$twilioToken="54533869bd38e7e6d9c83c1909c4f457";
+	$twilioSID="x";
+	$twilioToken="x";
 ?>
